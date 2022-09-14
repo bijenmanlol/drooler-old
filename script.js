af562da01137e5ff0a214c798bfc41a05fc3dfd1
bijenmanlol <85669046+bijenmanlol@users.noreply.github.com>
2022-09-14 18:06:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+if(window.location.href === "https://www.drooler.tk/"){
+  
+  window.location.href = "https://drooler.tk/";
+  
+}
+
+function loaded() {
+  
+  document.getElementsByTagName("body")[0].style.display = "inline";
+  
+}
+
+//Currently no rights reserved
